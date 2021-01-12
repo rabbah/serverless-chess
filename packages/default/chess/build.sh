3aea679b9003336b247cdb5e56e6f7e8c1d5aa73
Rodric Rabbah <rodric@gmail.com>
2021-01-12 03:06:15 -0500

    Fix build path.

--- packages/default/chess/build.sh
+++ packages/default/chess/build.sh
@@ -7,5 +7,6 @@ echo "package main" > main/index.go
 echo 'var indexHTML = `<!DOCTYPE html>' >> main/index.go
 cat web/index.html | sed 's/ + "api\/default\/chess"//' >> main/index.go
 echo '`;' >> main/index.go
-zip -r packages/default/chess/chess.go.zip main
+cd main
+zip -r ../packages/default/chess/chess.go.zip *
 popd
